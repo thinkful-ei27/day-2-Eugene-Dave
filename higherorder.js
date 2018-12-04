0f8d92de765407a3fb70a2dc761ad3d0822ef1d9
Eugene Song <yougene42193@gmail.com>
2018-12-04 16:16:57 -0500

    added forEach, filter and map

--- higherorder.js
+++ higherorder.js
@@ -66,3 +66,10 @@ function hazardWarningCreator(typeOfWarning){
     mooseWarning('Main St and Atlantic Ave');
     tsunamiWarning('Atlantic Ave and Pacific Ave');
 
+let movements = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+    movements = movements.filter(step => step[0] >= 0 && step[1] >= 0); 
+    steps = movements.map(step => step[0] + step[1]);
+        console.log(steps);
+    steps = movements.forEach(step => {
+        console.log(step);
+    })
